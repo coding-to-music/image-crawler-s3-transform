8a0edd400fb9550c3ec8b7c281460e85f9c02079
coding-to-music <connors.tom@gmail.com>
2022-11-15 22:17:29 +0000

    Build

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // server.js
 
-import UploadImageByUrl from './src/upload.js';
+import UploadImageByUrl from './src/upload.ts';
 
 const PrintResults = async () => {
   const url = 'https://ae01.alicdn.com/kf/HTB13gJEKeGSBuNjSspbq6AiipXaM.jpg';
