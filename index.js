0341ef454070f8e900ac8e32f94d597951388ca4
coding-to-music <connors.tom@gmail.com>
2022-11-15 22:08:10 +0000

    Build

---
+++ index.js
@@ -0,0 +1,27 @@
+// server.js
+
+import UploadImageByUrl from './src/upload.js';
+
+const PrintResults = async () => {
+  const url = 'https://ae01.alicdn.com/kf/HTB13gJEKeGSBuNjSspbq6AiipXaM.jpg';
+  const title = 'product image from ali express';
+
+  const { image, placeholder, error } = await UploadImageByUrl(url, title);
+
+  return { image, placeholder, error };
+};
+
+PrintResults();
+
+// Print Results:
+// {
+//   image: {
+//     path: '/2021/7/product_image_from_ali_express_1625320790_utZlhTnHo.jpg',
+//     ETag: '"fa8bc66b3d45370d5997856fb07cef07"'
+//   },
+//   placeholder: {
+//     path: '/2021/7/product_image_from_ali_express_1625320790_utZlhTnHo_placeholder.jpg',
+//     ETag: '"22436eaa7cd6c1b0ee25ec171265dcbc"'
+//   },
+//   error: undefined
+// }
